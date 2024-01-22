0d911471ac47c19a957780a257bdd057e437862f
Indrakka <96984424+Indrakka@users.noreply.github.com>
2024-01-22 17:01:14 +0530

    Add files via upload

---
+++ MainAppStringFunction.java
@@ -0,0 +1,19 @@
+package com.edu;
+
+public class MainAppStringFunction {
+
+	public static void main(String[] args) {
+		String s ="Hello";
+		
+		System.out.println("s="+s);
+		//no of charas
+		System.out.println("length="+s.length());
+		System.out.println("uppercase string ="+s.toUpperCase());
+		//lowercase
+		System.out.println("character at 2 "+s.charAt(1));
+		
+		
+
+	}
+
+}
